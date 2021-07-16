4ea8cb0df9781fac09a9ac246c5810de09ebf3ab
DJ Meph <norm.witte@gmail.com>
2021-07-16 00:59:41 -0400

    Split config header and create task scheduler framework

--- include/config.h
+++ include/config.h
@@ -1,9 +1,41 @@
+#include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
 #include <ArduinoJson.h>
+#include <Audio.h>
+#include <TaskScheduler.h>
 
-const char configFileName[13] = "CONFJSON.TXT";
+#define echoPin 41 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 40 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define led 13 // Built-in LED
+#define configFileName "CONFJSON.TXT"
+
+void readDistance();
+void taskMonitor();
+void errorBlink();
+
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+enum Stage { pitch, cta, dispense };
+
+Scheduler scheduler;
+Task userDistance(10, TASK_FOREVER, &readDistance);
+Task monitor(1000, TASK_FOREVER, &taskMonitor);
+Stage stage;
+
+// GUItool: begin automatically generated code
+AudioPlaySdWav           playWav;        //xy=864,427
+AudioAmplifier           amp0;           //xy=1081,400
+AudioAmplifier           amp1;           //xy=1084,440
+// AudioOutputAnalog        dac;            //xy=1313,421
+AudioOutputMQS           dac;           //xy=1258,427
+AudioConnection          patchCord1(playWav, 0, amp0, 0);
+AudioConnection          patchCord2(playWav, 1, amp1, 0);
+AudioConnection          patchCord3(amp0, 0, dac, 0);
+AudioConnection          patchCord4(amp1, 0, dac, 1);
+// GUItool: end automatically generated code
 
 StaticJsonDocument<128> readConfig() {
   /**
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,60 @@
-#include <Wire.h>
 #include <config.h>
-#include <play.h>
 
-uint8_t led = 13; // Built-in LED
+void playFile(char filename[]) {
+  Serial.printf("Playing file: %s\n", filename);
+  if (SD.exists(filename)) {
+    playWav.play(filename);
+    delay(5);
+
+    while (playWav.isPlaying()) {
+      Serial.printf("Elapsed milliseconds: %d\n", playWav.positionMillis());
+      delay(500);
+    }
+  } else {
+    Serial.printf("Error: File (%s) not found ...\n", filename);
+    errorBlink();
+  }
+}
+
+void readDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+}
+
+void taskMonitor() {
+  Serial.printf("Distance: %d cm\n", distance);
+}
+
+void errorBlink() {
+  digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
+  delay(100);
+  digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
+  delay(100);
+  digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
+  delay(100);
+  digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
+}
 
 void setup() {
   Serial.begin(9600);
   while (!Serial) continue;
 
   pinMode(led, OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
   AudioMemory(128);
   amp0.gain(1);
   amp1.gain(1);
@@ -19,23 +65,33 @@ void setup() {
     return;
   }
   Serial.println("initialization done.");
+
+  scheduler.addTask(userDistance);
+  scheduler.addTask(monitor);
+  userDistance.enable();
+  monitor.enable();
 }
 
 void loop() {
-  StaticJsonDocument<128> config = readConfig();
+  scheduler.execute();
+  // StaticJsonDocument<128> config = readConfig();
 
-  uint8_t pitchCount = config["pitch"].size();
-  uint8_t insertCoinCount = config["insertCoin"].size();
-  uint8_t pressButtonCount = config["pressButton"].size();
-  uint8_t outOfCardsCount = config["outOfCards"].size();
+  // uint8_t pitchCount = config["pitch"].size();
+  // uint8_t insertCoinCount = config["insertCoin"].size();
+  // uint8_t pressButtonCount = config["pressButton"].size();
+  // uint8_t outOfCardsCount = config["outOfCards"].size();
 
-  Serial.printf("Size of pitches array: %u\n", pitchCount);
-  Serial.printf("Size of insertCoin array: %u\n", insertCoinCount);
-  Serial.printf("Size of pressButton array: %u\n", pressButtonCount);
-  Serial.printf("Size of outOfCards array: %u\n", outOfCardsCount);
+  // Serial.printf("Size of pitches array: %u\n", pitchCount);
+  // Serial.printf("Size of insertCoin array: %u\n", insertCoinCount);
+  // Serial.printf("Size of pressButton array: %u\n", pressButtonCount);
+  // Serial.printf("Size of outOfCards array: %u\n", outOfCardsCount);
 
-  const JsonVariantConst variant = config["pitch"][0];
-  const char* selectPitch = variant;
+  // while (stage == pitch) {
+  //   pitchFn();
+  // }
+
+  // const JsonVariantConst variant = config["pitch"][0];
+  // const char* selectPitch = variant;
 
   /**
    * If I pass `selectPitch` directly to playFile() the name of the file prints (line 66)
@@ -44,9 +100,9 @@ void loop() {
    * playFile(), everything works. Feels like I'm doing something wrong here.
    */
 
-  char selectionCopy[strlen(selectPitch)] = {};
-  strcpy(selectionCopy, selectPitch);
+  // char selectionCopy[strlen(selectPitch)] = {};
+  // strcpy(selectionCopy, selectPitch);
 
-  playFile(selectionCopy, led);
-  delay(5000);
+  // playFile(selectionCopy, led);
+  // delay(5000);
 }
